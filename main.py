d44a83c6bf20408ce49d1d2c66abdbdb733648da
Даниил <152735181+DrTECHNIC@users.noreply.github.com>
2024-10-25 14:54:50 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -5,20 +5,19 @@ from terminal import Terminal
 import json
 
 def main():
-    #if len(argv) > 1:
-    #    config_file = f"{argv[1:]}"
-    config_file = "config.json"
-    try:
-        with open(config_file, "r", encoding="UTF-8") as file:
-            data = json.load(file)
-            username = data.get("username")
-            filesystem_path = data.get("filesystem_path")
-            with TarFile(filesystem_path, 'a') as file_system:
-                Application(Terminal(username, filesystem_path, file_system))
-    except FileNotFoundError:
-        print(f"Файл {config_file} не найден.")
-    #else:
-    #    print("Аргументы не были переданы.")
+    if len(argv) > 1:
+        config_file = argv[1]
+        try:
+            with open(config_file, "r", encoding="UTF-8") as file:
+                data = json.load(file)
+                username = data.get("username")
+                filesystem_path = data.get("filesystem_path")
+                with TarFile(filesystem_path, 'a') as file_system:
+                    Application(Terminal(username, filesystem_path, file_system))
+        except FileNotFoundError:
+            print(f"Файл {config_file} не найден.")
+    else:
+        print("Аргументы не были переданы.")
 
 if __name__ == "__main__":
     main()
